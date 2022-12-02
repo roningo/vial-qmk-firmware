01157d60829ea50abc823cc12015434af8dff35c
roningo <laekoroningo@gmail.com>
2022-12-02 13:39:59 +0700

    update

--- keyboards/alice51/keymaps/vial/keymap.c
+++ keyboards/alice51/keymaps/vial/keymap.c
@@ -13,13 +13,27 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT]=LAYOUT(
-        KC_B, KC_A, KC_A, KC_A, KC_A, KC_A, 		KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_C,
-        KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, 		KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A,
-        KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, 		KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A,
-        KC_E, KC_A, KC_A, KC_A, KC_A,       KC_A,         KC_A, KC_A, KC_A, KC_A, KC_A, KC_D
+        KC_ESC,  KC_Q, KC_W, KC_E, KC_R,   KC_T,        KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC, KC_BSPC,
+        KC_TAB,  KC_A, KC_S, KC_D, KC_F,   KC_G,        KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V,   KC_B,        KC_B, KC_N, KC_M, KC_COMM, KC_SLSH, KC_UP,   KC_ENT,
+        KC_LCTL, KC_A, KC_A, KC_A, KC_LALT,      KC_SPC,      KC_A, KC_A, KC_A,    KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 	
 	[FN1]=LAYOUT(
+        ____, ____, ____, ____, ____, ____, 		 ____, ____, ____, ____, ____, KC_LBRC, KC_DEL,
+        KC_CAPS, ____, ____, ____, ____, ____, 		 ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____, ____, 		 ____, ____, ____, KC_DOT, ____, ____, ____,
+        ____, ____, ____, ____, ____,       ____,          ____, ____, ____, ____, ____, ____
+    ),
+	
+	[FN2]=LAYOUT(
+        ____, ____, ____, ____, ____, ____, 		 ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____, ____, 		 ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____, ____, 		 ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____,       ____,          ____, ____, ____, ____, ____, ____
+    ),
+	
+	[FN3]=LAYOUT(
         ____, ____, ____, ____, ____, ____, 		 ____, ____, ____, ____, ____, ____, ____,
         ____, ____, ____, ____, ____, ____, 		 ____, ____, ____, ____, ____, ____, ____,
         ____, ____, ____, ____, ____, ____, 		 ____, ____, ____, ____, ____, ____, ____,
